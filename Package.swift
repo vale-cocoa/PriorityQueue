19043e9362776aa962d59af51a2c78abaa6b3f80
Valeriano Della Longa <valeriano.dellalonga@me.com>
2020-11-01 19:19:47 +0100

    Now using Queue package v1.0.1

--- Package.swift
+++ Package.swift
@@ -14,16 +14,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/vale-cocoa/HeapBuffer.git", from: "1.0.7")
+        .package(url: "https://github.com/vale-cocoa/HeapBuffer.git", from: "1.0.7"),
+        .package(url: "https://github.com/vale-cocoa/Queue.git", from: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PriorityQueue",
-            dependencies: ["HeapBuffer"]),
+            dependencies: ["HeapBuffer", "Queue"]),
         .testTarget(
             name: "PriorityQueueTests",
-            dependencies: ["PriorityQueue", "HeapBuffer"]),
+            dependencies: ["PriorityQueue", "HeapBuffer", "Queue"]),
     ]
 )
